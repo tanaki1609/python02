6e75be3eaaad6e9840e956291b0dd9b4d53e4a19
tanaki93 <tanaki.1609@gmail.com>
2021-02-06 15:07:28 +0600

    added binary search algorithm

--- algorithms/recurse.py
+++ algorithms/recurse.py
@@ -1,8 +1,10 @@
-def recurse(num):
-    if num > 100:
-        return
-    num += 10
-    print(num)
-    recurse(num)
+def fibo(n, a=0, b=1, fib=[]):
+    if b <= n:
+        fib.append(b)
+        a, b = b, a + b
+    else:
+        return fib
+    return fibo(n, a, b, fib)
 
-recurse(0)
+
+print(fibo(8))
